e2bb87bc9450007d2f98332d5fe9e9746156c967
Jordan Kell <kellrond@gmail.com>
2022-10-18 21:24:45 -0700

    add to camera todo file

--- modules/camera.py
+++ modules/camera.py
@@ -1,28 +1,45 @@
+''' The camera module should contain all of the functionality related to getting and storing
+    images in the system.
+
+    A few things for the todo list
+
+    General camera function:
+        - detect, identify and get meta data about cameras connected
+        - List any cameras currently in use
+        - Register a camera with the system
+
+    Camera functionality:
+        - Take image
+        - Schedule image
+        - timer image
+        - interval image
+        - record video
+        - detect motion
+        - generate stream
+
+    Camera parameters:
+        - resolution
+        - quality 
+        - dimensions
+        - color
+        - contrast
+        - brightness
+'''
+
+
+from time import sleep
 import cv2
 
 
 def runCameraTest():
-    cam = cv2.VideoCapture(0)
-    # title of the app
-    # cv2.namedWindow('python webcam screenshot app')
-
-    # # let's assume the number of images gotten is 0
     img_counter = 0
+    while True:
+        cam = cv2.VideoCapture(0)
 
+        ret, frame = cam.read()
+        
+        img_name = f'data/images/image_{img_counter}.png' 
+        cv2.imwrite(img_name, frame)
+        img_counter += 1
 
-    ret, frame = cam.read()
-
-
-    # the format for storing the images scrreenshotted
-    img_name = f'opencv_frame_{img_counter}.jpg'
-    # saves the image as a png file
-    cv2.imwrite(img_name, frame)
-    print('screenshot taken')
-    # the number of images automaticallly increases by 1
-    img_counter += 1
-
-    # release the camera
-    cam.release()
-
-    # # stops the camera window
-    # cam.destoryAllWindows()
+        cam.release()
